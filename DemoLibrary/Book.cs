12566de9ee269069f4857b326a2a399bac1a4ee2
Mdsomratakbor <somrat876@gmail.com>
2020-06-22 21:10:49 +0600

    IAudioBook interface add

--- DemoLibrary/Book.cs
+++ DemoLibrary/Book.cs
@@ -6,7 +6,29 @@ using System.Threading.Tasks;
 
 namespace DemoLibrary
 {
-    public class Book
+    public class Book :IBorrowableBook
     {
+      public string LibraryID { get; set; }
+        public DateTime BorrowDate { get; set; }
+        public string Borrower { get; set; }
+        public int CheckOutDurationInDays { get; set; }
+        public string Author { get; set; }
+        public int Pages { get; set; }
+        public string Title { get; set; }
+
+        public void CheckIn()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CheckOut(string borrower)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DateTime GetDueDate()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- DemoLibrary/ReferenceBook.cs
+++ DemoLibrary/ReferenceBook.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace DemoLibrary
 {
-    public class ReferenceBook
+    public class ReferenceBook : IBook
     {
+        public string LibraryID { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Pages { get; set; }
+  
+      
     }
 }
