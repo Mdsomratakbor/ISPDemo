50ad5404e112bbb272b3f32d3af24daccb47db9d
Mdsomratakbor <somrat876@gmail.com>
2020-06-22 21:21:57 +0600

    IBorrowableDVD interface  add

--- DemoLibrary/DVD.cs
+++ DemoLibrary/DVD.cs
@@ -6,7 +6,30 @@ using System.Threading.Tasks;
 
 namespace DemoLibrary
 {
-    public class DVD
+    public class DVD : IBorrowabeDVD
     {
+        public string LibraryID { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Title { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public List<string> Actors { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int RuntimeInMinutes { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+  
+        public DateTime BorrowDate { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Borrower { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int CheckOutDurationInDays { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public void CheckIn()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CheckOut(string borrower)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DateTime GetDueDate()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
