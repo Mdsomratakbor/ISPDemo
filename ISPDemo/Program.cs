7adb03ba95dc2351d6297a47f8f39dde651eb4aa
Mdsomratakbor <somrat876@gmail.com>
2020-06-22 21:32:38 +0600

    Folder add

--- ISPDemo/Program.cs
+++ ISPDemo/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DemoLibrary;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,10 @@ namespace ISPDemo
     {
         static void Main(string[] args)
         {
+            IBorrowabeDVD dvd = new DVD();
+            
+
+            Console.Read();
         }
     }
 }
